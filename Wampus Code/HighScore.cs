15941626abc45083b3c72575293f4598fb289676
Jason <jason.a.miller@live.com>
2020-02-24 09:34:37 -0800

    Added stuff to high score class

--- Wampus Code/HighScore.cs
+++ Wampus Code/HighScore.cs
@@ -6,5 +6,25 @@ using System.Threading.Tasks;
 
 namespace Wampus_Warriors.Wampus_Code {
     class HighScore {
+
+        private static String SAVE_FILE_LOCATION;
+        private static String NEW_SCORE_LOCATION;
+        private static String BACKUP_SCORE_LOCATION;
+
+        private static List<User> users;
+        public HighScore() {
+
+        }
+
+        public String [] getHighScores() {
+            String[] array = new string[5];
+            return array;
+        }
+
+        public void addHighScore(int score) {
+
+        }
+
+
     }
 }
