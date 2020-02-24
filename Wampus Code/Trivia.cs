aec9e39e844045696fbd2df2d530810f0ec7b460
Jason <jason.a.miller@live.com>
2020-02-24 13:50:44 -0800

    Added trivia class to the project

--- Wampus Code/Trivia.cs
+++ Wampus Code/Trivia.cs
@@ -5,33 +5,28 @@
  * 2.20.2020
  * Trivia Object
  */
+namespace Wampus_Warriors.Wampus_Code {
+    public class Trivia {
+        public Trivia() {
 
-public class Trivia
-{
-	public Trivia()
-	{
-        
-	}
-    // instance of corr
-    private bool verity;
-    public bool buyArrows()
-    {
-        //reads from file 3 questions, if 2 or more are answered right: return true, else: return false.
-        return false;
-    }
-    public bool buySecret()
-    {
-        //reads from file 3 questions, if 2 or more answered right: return true, else: return false.
-        return false;
-    }
-    public bool savePit()
-    {
-        //reads from file 3 questions, if 2 or more are answered right: return true, else: return false.
-        return false;
-    }
-    public bool escapeWumpus()
-    {
-        //reads from file 5 questions, if 3 or more are answered right: return true, else: return false.
-        return false;
+        }
+        // instance of corr
+        private bool verity;
+        public bool buyArrows() {
+            //reads from file 3 questions, if 2 or more are answered right: return true, else: return false.
+            return false;
+        }
+        public bool buySecret() {
+            //reads from file 3 questions, if 2 or more answered right: return true, else: return false.
+            return false;
+        }
+        public bool savePit() {
+            //reads from file 3 questions, if 2 or more are answered right: return true, else: return false.
+            return false;
+        }
+        public bool escapeWumpus() {
+            //reads from file 5 questions, if 3 or more are answered right: return true, else: return false.
+            return false;
+        }
     }
 }
