ab916c0ffec0a15a2b45ce0da07f1efc22f8aeb1
DanFirstenberg <60745596+DanFirstenberg@users.noreply.github.com>
2020-02-24 22:52:49 -0800

    Stubbed Player Object class

--- Wampus Code/Player.cs
+++ Wampus Code/Player.cs
@@ -1,4 +1,47 @@
-﻿namespace Wampus_Warriors.Wampus_Code {
-    public class Player {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * GameLocation.C# 
+ * by Dan Firstenberg
+ * 2/21/2020
+ * Period 3
+ */
+
+namespace GameLocation
+{
+    class Player
+    {
+        private int arrows = 0;
+        private int goldCoins = 0;
+        private int turns = 0;
+        private int score = 0;
+
+        public int numberOfArrows()
+        {
+            // keeps track of the number of arrows the player currently has
+            return arrows;
+        }
+
+        public int numberOfGoldCoins()
+        {
+            // keeps track of the number of gold coins the player currently has
+            return goldCoins;
+        }
+
+        public int numberOfTurns()
+        {
+            // keeps track of the number of turns the player has taken
+            return turns;
+        }
+
+        public int finalScore()
+        {
+            // keeps track of the score throughout the game
+            return score;
+        }
     }
 }
