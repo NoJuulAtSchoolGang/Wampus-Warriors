62035f6061dd955e48e32cca4c6cae6a078e3e83
Jason <jason.a.miller@live.com>
2020-02-24 16:46:04 -0800

    Revised high score class

--- Wampus Code/HighScore.cs
+++ Wampus Code/HighScore.cs
@@ -7,20 +7,26 @@ using System.Threading.Tasks;
 namespace Wampus_Warriors.Wampus_Code {
     class HighScore {
 
+        //constants for file locations
         private static String SAVE_FILE_LOCATION;
         private static String NEW_SCORE_LOCATION;
         private static String BACKUP_SCORE_LOCATION;
 
+        //list that contains data for users
         private static List<User> users;
-        public HighScore() {
 
+        //constructor
+        public HighScore() {
+            users = new List<User>();
         }
 
+        //method to return current high scores as a string
         public String [] getHighScores() {
             String[] array = new string[5];
             return array;
         }
 
+        //method to add high score
         public void addHighScore(int score) {
 
         }
