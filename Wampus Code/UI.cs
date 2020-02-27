666b4e9f8527cd637f6ec0093d65e394f2616188
Jason Miller <jason.a.miller@live.com>
2020-02-27 10:38:34 -0800

    Filled in drawplayer method

--- Wampus Code/UI.cs
+++ Wampus Code/UI.cs
@@ -5,23 +5,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Drawing;
 namespace Wampus_Warriors.Wampus_Code {
     class UI {
 
+        private Image playerUp;
+        private Image playerDown;
+        private Image playerLeft;
+        private Image playerRight;
+        private Size canvas_size;
 
-        public UI() {
+        public UI(Size canvas_size) {
+            playerUp = Image.FromFile("playerUp.jpg");
+            playerDown = Image.FromFile("playerDown.jpg");
+            playerLeft = Image.FromFile("playerLeft.jpg");
+            playerRight = Image.FromFile("playerRight.jpg");
 
+            this.canvas_size = canvas_size;
         }
         /**
          * These are the methods used for rendering the game objects
         **/
 
         //will draw the player using information from the player class
-        public void drawPlayer(Player player, Graphics g) {}
+        public void drawPlayer(Player player, String direction, Graphics g) {
+            int x = player.getX();
+            int y = player.getY();
+
+            Image playerImage;
+            if (direction.Equals("up")){
+                playerImage = playerUp;
+            }
+            if (direction.Equals("down")) {
+                playerImage = playerDown;
+            }
+            if (direction.Equals("left")) {
+                playerImage = playerLeft;
+            }
+            if (direction.Equals("right")) {
+                playerImage = playerRight;
+            }
+
+            Rectangle playerRect = new Rectangle(x, y, this.canvas_size.Width/20, this.canvas_size.Width/20);
+
+            g.DrawImage(playerImage, playerRect);
+        }
 
         //will draw the current cave using information from the GameLocation class
-        public void drawCave(Cave cave, Graphics g) {}
+        public void drawCave(GameLocation location, Graphics g) {
+            
+
+        }
 
         public void drawWampus(bool wampusInRoom, Graphics g) {}
 
